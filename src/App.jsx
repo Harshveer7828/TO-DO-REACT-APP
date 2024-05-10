472c6d00c127826c083e0620de2554fa7c693927
harshveer7828 <harshveer7828@gmail.com>
2024-05-10 20:54:59 +0530

    To-do create Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,61 @@
 import { useState } from 'react'
 import './index.css';
 import 'remixicon/fonts/remixicon.css'
-import 'nanoid'; 
 import { nanoid } from 'nanoid';
 
 function App() {
-  let [data,setData] = useState(JSON.parse(localStorage.getItem("task")) || []);
-  let [task,setTask] = useState("");
+  let [data, setData] = useState(JSON.parse(localStorage.getItem("task")) || []);
+  let [tasks, setTasks] = useState("");
 
-  const formHandler = (e)=>{
+  const formHandler = (e) => {
     e.preventDefault();
-    const newData = {task,complete : "false",id: nanoid() };
-    setData(...data,newData);
-    
+    const newData = { tasks, complete: "false", id: nanoid() };
+    setData([...data, newData]);
+    setTasks("");
+
+    localStorage.setItem("task", JSON.stringify([...data, newData]));
+
   }
+
+  const CompleteHandler = (index) => {
+    const copyTasks = [...data];
+    copyTasks[index].complete = !copyTasks[index].complete;
+    setData(copyTasks)
+    localStorage.setItem("task", JSON.stringify(copyTasks))
+  }
+  console.log(data);
 
 
   return (
     <>
-      <div className="container h-screen w-screen bg-zinc-900 flex items-center justify-center flex-col">
+      <div className="container min-h-screen w-screen bg-zinc-900 flex items-center justify-center flex-col">
         <div className="information h-[20vh] w-[90%] border-2 border-zinc-200 flex items-center justify-between px-4">
           <div className="text">
             <h1 className="text-white text-6xl font-bold font-['manuka']">Let's ToDo</h1>
             <h3 className="text-white text-3xl font-['manuka']">Keep Doing Thing's</h3>
           </div>
-          <h3 className="text-8xl text-white font-['manuka']">7/0</h3>
+          <h3 className="text-8xl text-white font-['manuka']">{data.filter(t => t.complete === true).length}/{data.length} </h3>
         </div>
         <div className="inputContainer">
           <form onSubmit={formHandler} className="flex items-center justify-center mt-2 gap-2">
-            <input onChange={(e)=> setTask(e.target.value)} value={task} type="text" name="tasks" className="border  rounded-md  text-white bg-transparent py-2 px-1" placeholder='Enter your task' />
+            <input onChange={(e) => setTasks(e.target.value)} value={tasks} type="text" name="tasks" className="border  rounded-md  text-white bg-transparent py-2 px-1" placeholder='Enter your task' />
             <button className="px-3 text-white text-3xl py-1 bg-red-500 rounded-full">+</button>
           </form>
         </div>
         <div className="mt-2 tasksList px-2 min-h-[10vh] w-full flex flex-col gap-2">
-          <div className="flex items-center justify-between task w-full border border-zinc-400 text-white px-2 py-3">
-            <div className='flex items-center gap-2'>
-              <div className="check h-[1.5rem] w-[1.5rem] rounded-full bg-green-400"></div>
-              <h1 className='line-through	'>This is my tasks</h1>
-            </div>
-            <div className="controls "><i className="ri-delete-bin-2-line"></i></div>
-          </div>
-          <div className="flex items-center justify-between task w-full border border-zinc-400 text-white px-2 py-3">
-            <div className='flex items-center gap-2'>
-              <div className="check h-[1.5rem] w-[1.5rem] rounded-full bg-red-400"></div>
-              <h1>This is my tasks</h1>
-            </div>
-            <div className="controls "><i className="ri-delete-bin-2-line"></i></div>
-          </div>
+          {data.length > 0 ? (
+            data.map((tasks, index) => {
+              return (<div key={tasks.id} className="flex items-center justify-between task w-full border border-zinc-400 text-white px-2 py-3">
+                <div onClick={() => CompleteHandler(index)} className='flex items-center gap-2'>
+                  <div className={`check h-[1.5rem] w-[1.5rem] rounded-full ${tasks.complete ? "bg-green-400" : "border border-red-500"}`}></div>
+                  <h1 className={`${tasks.complete ? "line-through" : 'none'}`}>{tasks.tasks}</h1>
+                </div>
+                <div className="controls "><i className="ri-delete-bin-2-line"></i></div>
+              </div>)
+            })
+          ) : (
+            <h1 className="text-white text-4xl font-[''manuka] text-center">No Pending Tasks</h1>
+          )}
         </div>
       </div>
     </>
